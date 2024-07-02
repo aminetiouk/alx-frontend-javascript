5dff6d4661157af4990029b8a03dbb7abcd00904
“Am91T” <tiouk.amine@gmail.com>
2024-07-03 00:22:22 +0100

    Done

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,10 @@
-const updateUniqueItems = (map) => {
-  if (!(map instanceof Map)) throw new Error('Cannot process');
-
-  map.forEach((value, key) => {
-    if (value === 1) map.set(key, 100);
-  });
-};
-
-export default updateUniqueItems;
+function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error("Cannot process");
+  }
+  for (let [key, value] of map.entries()) {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  }
+}
